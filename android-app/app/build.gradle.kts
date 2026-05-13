b6648e0927711f96f2e8627a26b1e22403157d5a
nickgr83 <nick.gressens@gmail.com>
2026-05-13 15:20:31 +0200

    updated theme

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -44,6 +44,16 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+	
 }
 
 dependencies {
