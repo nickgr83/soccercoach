65a58c7241f122f73616eb77d72ee81ba2dba0a1
nickgr83 <nick.gressens@gmail.com>
2026-05-13 15:13:53 +0200

    updated theme

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -65,4 +65,9 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // ✅ ADD THIS
+    implementation("com.google.android.material:material:1.12.0")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
